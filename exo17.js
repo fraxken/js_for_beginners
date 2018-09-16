693572153e83f413feaf99787e0fc09fb6f1adbd
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-16 20:37:03 +0200

    Ajout d'un exo n17

---
+++ exo17.js
@@ -0,0 +1,23 @@
+/**
+ * @namespace EXO17
+ * 
+ * @desc
+ * Supprimer les doublons et reconstruiser une chaîne de caractères à partir du tableau filtré.
+ * 
+ * @tutorial (documentation)
+ * - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/join
+ * 
+ * @todo (Consignes de l'exercice)
+ * 1. Supprimer les doublons du tableau "valeurs"
+ * 2. Faite une concatenation du tableau "valeurs" en chaine de caractères avec le séparateur suivant ", "
+ * 3. Afficher le résultat dans la console!
+ * 
+ * @see
+ * En JavaScript moderne, il est possible de réaliser cet exercice avec une Structure Set:
+ * - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Set
+ */
+
+// --> Tableau contenant des couleurs (certaine en doublon!)
+const valeurs = ["rouge", "vert", "bleue", "rouge", "rose", "vert", "violet", "jaune"];
+
+// --> Continuer le code ci-dessous en suivant les consignes de l'exercice ci-dessus !
