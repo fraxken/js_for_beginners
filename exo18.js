6b088873540fdd5f5d19961e3d8888d8db91231c
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-16 23:05:40 +0200

    Ajout de l'exercice 18

---
+++ exo18.js
@@ -0,0 +1,20 @@
+/**
+ * @namespace EXO18
+ * 
+ * @desc
+ * Simplifier le code en utilisant une fonction. (Dans l'objectif d'éviter une répétition).
+ * 
+ * @tutorial (documentation)
+ * 
+ * @todo (Consignes de l'exercice)
+ * 1. Création d'une fonction pour multiplier une valeur par 2.
+ * 2. Utilisation de la fonction aux travers de nos différentes opérations.
+ */
+
+// --> Continuer le code ci-dessous en suivant les consignes de l'exercice ci-dessus !
+
+// --> Optimiser/Simplifier les instructions suivantes
+console.log([1, 2, 4].map((int) => int * 2));
+console.log([1, 5, 10, 4].map((int) => int * 2));
+console.log([25, 10, 55].map((int) => int * 2));
+console.log([100, -10, 5, 2].map((int) => int * 2));
