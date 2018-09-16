62730f18cf4ed1acb4eb1233848b008d46ee4499
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-16 20:36:41 +0200

    Ajout d'un exo n15

---
+++ exo15.js
@@ -0,0 +1,25 @@
+/**
+ * @namespace EXO15
+ * 
+ * @desc
+ * Création d'un tableau "C" contenant les valeurs du tableau "A" et "B"
+ * 
+ * @tutorial (documentation)
+ * - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/push
+ * 
+ * @todo (Consignes de l'exercice)
+ * 1. Création d'un tableau portant le nom "C" avec les valeurs du tableau "A" et "B"
+ * 
+ * @see
+ * En JavaScript moderne il est possible d'effectuer cet exercice à l'aide de l'opérateur "Spread":
+ * - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/Syntaxe_d%C3%A9composition
+ * 
+ * Essayez donc !
+ */
+
+// --> Deux tableaux avec des valeurs diverses (polymorphe)
+const A = ["hello", 5, true];
+const B = [19, 244, false, "mer il est fou!"];
+
+// --> Continuer le code ci-dessous en suivant les consignes de l'exercice ci-dessus !
+const C = [];
