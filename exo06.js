8af5884138d25d4beab07262e629a63ad675055c
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-17 20:12:50 +0200

    Fix typo for exo06 et exo08

--- exo06.js
+++ exo06.js
@@ -2,9 +2,9 @@
  * @namespace EXO06
  * 
  * @desc
- * Multiplier toutes les valeurs d'un tableau par 2 et filtrer pour ne récupérer que les valeurs supérieures à vingt. (valeur > 20);
+ * Multiplier toutes les valeurs d'un tableau par deux et filtrer pour ne récupérer que les valeurs supérieures à vingt.
  * 
- * Il existe plusieurs façons d'aboutir au résultat attendu (Néanmoins nous vous conseillons d'utiliser les méthodes map et filter).
+ * Il existe plusieurs façons d'aboutir au résultat attendu (Nous vous conseillons d'utiliser les méthodes map et filter).
  * 
  * @tutorial (documentation)
  * - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/map
@@ -13,15 +13,15 @@
  * Les méthodes "map" et "filter" prenne une fonction en argument. Les documentations utilisent souvent une annotation comme ci-dessous:
  * (x) => x.length
  * 
- * Ce n'est rien de plus qu'une fonction qui peut être retraduit en:
+ * Ce n'est rien de plus qu'une fonction qui peut être retraduit :
  * function(x) {
  *     return x.length;
  * }
  * 
  * @todo (Consignes de l'exercice)
- * 1. Multiplier toutes les valeurs du tableau "valeurs" par deux.
- * 2. Filtrer le tableau pour ne récupérer que les valeurs supérieur à vingt.
- * 3. Afficher dans le terminal le résultat!
+ * 1. Multiplier toutes les valeurs du tableau "tableauDeNumbers" par deux.
+ * 2. Filtrer le tableau pour ne récupérer que les valeurs supérieures à vingt.
+ * 3. Afficher le résultat dans le terminal.
  * 
  * @see
  * N'hésitez pas à refaire l'exercice avec une boucle "for" classique pour vous entrainer.
@@ -34,6 +34,6 @@
  */
 
 // Un tableau contenant des entiers (nombre).
-const valeurs = [2, 5, 10, 15, 20, 40];
+const tableauDeNumbers = [2, 5, 10, 15, 20, 40];
 
 // --> Code ici en suivant les consignes de l'exercice ci-dessus !
--- exo08.js
+++ exo08.js
@@ -14,7 +14,7 @@
  * B. Afficher le message "mer il est fou!" si la valeur de "valeurRandom" est fausse (false).
  * 
  * @see
- * Il est aussi possible de développer une solution plus "courte" avec les "ternaires". Essayez !
+ * Il est aussi possible de développer une solution plus "courte" avec les "ternaires". Essayez donc !
  * - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/L_op%C3%A9rateur_conditionnel
  */
 
